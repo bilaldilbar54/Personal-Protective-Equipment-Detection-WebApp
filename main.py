0a769cd02c97bbbae2446128d0e93daa9073ea45
Bilal Dilbar <bilal.dilbar54@gmail.com>
2024-04-20 00:14:47 -0400

    Updated Readme.md

--- main.py
+++ main.py
@@ -183,4 +183,5 @@ def contact_us():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(debug=True)
+    # app.run(host='0.0.0.0', port=8080, debug=True)
